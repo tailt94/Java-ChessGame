81c59f4766752f9a15b4a14046e43475edcee675
Lê Tuấn Tài <tuantai0625@gmail.com>
2017-06-13 20:25:57 +0700

    Add pieces

--- src/com/tuantai0625/chessgame/controller/MainController.java
+++ src/com/tuantai0625/chessgame/controller/MainController.java
@@ -5,6 +5,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.GridPane;
@@ -35,12 +36,63 @@ public class MainController implements Initializable{
         for (int row = 0; row < 8; row++) {
             for (int col = 0; col < 8; col++) {
                 Pane pane = new Pane();
+                addPiece(pane, row, col);
                 if ((row % 2 == 0 && col % 2 == 1) || (row % 2 == 1 && col % 2 == 0)) {
                     pane.setStyle("-fx-background-color: #33939A");
                 } else {
                     pane.setStyle("-fx-background-color: #98DCE1");
                 }
                 chessBoard.add(pane, col, row);
+            }
+        }
+    }
+
+    private void addPiece(Pane pane, int row, int col) {
+        if (row == 0) {
+            switch (col) {
+                case 0:
+                case 7:
+                    pane.getChildren().add(new ImageView("com/tuantai0625/chessgame/assets/pieces/black_rook.png"));
+                    break;
+                case 1:
+                case 6:
+                    pane.getChildren().add(new ImageView("com/tuantai0625/chessgame/assets/pieces/black_knight.png"));
+                    break;
+                case 2:
+                case 5:
+                    pane.getChildren().add(new ImageView("com/tuantai0625/chessgame/assets/pieces/black_bishop.png"));
+                    break;
+                case 3:
+                    pane.getChildren().add(new ImageView("com/tuantai0625/chessgame/assets/pieces/black_queen.png"));
+                    break;
+                case 4:
+                    pane.getChildren().add(new ImageView("com/tuantai0625/chessgame/assets/pieces/black_king.png"));
+                    break;
+            }
+        } else if (row == 1) {
+            pane.getChildren().add(new ImageView("com/tuantai0625/chessgame/assets/pieces/black_pawn.png"));
+        } else if (row == 6) {
+            pane.getChildren().add(new ImageView("com/tuantai0625/chessgame/assets/pieces/white_pawn.png"));
+        } else if (row == 7) {
+            switch (col) {
+                case 0:
+                case 7:
+                    pane.getChildren().add(new ImageView("com/tuantai0625/chessgame/assets/pieces/white_rook.png"));
+                    break;
+                case 1:
+                case 6:
+                    pane.getChildren().add(new ImageView("com/tuantai0625/chessgame/assets/pieces/white_knight.png"));
+                    break;
+                case 2:
+                case 5:
+                    pane.getChildren().add(new ImageView("com/tuantai0625/chessgame/assets/pieces/white_bishop.png"));
+                    break;
+                case 3:
+                    pane.getChildren().add(new ImageView("com/tuantai0625/chessgame/assets/pieces/white_queen.png"));
+                    break;
+                case 4:
+                    pane.getChildren().add(new ImageView("com/tuantai0625/chessgame/assets/pieces/white_king.png"));
+                    break;
             }
         }
     }
---
+++ src/com/tuantai0625/chessgame/model/ChessBoard.java
@@ -0,0 +1,7 @@
+package com.tuantai0625.chessgame.model;
+
+/**
+ * Created by Lionheart on 13-Jun-17.
+ */
+public class ChessBoard {
+}
