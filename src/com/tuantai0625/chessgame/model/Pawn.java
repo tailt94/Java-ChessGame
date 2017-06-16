02491fa36bc790235b76634748b0ae2c853ebf5d
Lê Tuấn Tài <tuantai0625@gmail.com>
2017-06-16 19:15:20 +0700

    Update Pawn legal move check

--- src/com/tuantai0625/chessgame/model/Pawn.java
+++ src/com/tuantai0625/chessgame/model/Pawn.java
@@ -15,6 +15,54 @@ public class Pawn extends Piece{
 
     @Override
     public boolean isLegalMove(ChessBoard b, int newRow, int newCol) {
-        return true;
+        int oldRow = this.tileOn.getRow();
+        int oldCol = this.tileOn.getCol();
+
+        /*
+         * Kiểm tra ô được di chuyển tới có tồn tại quân cờ cùng phe hay không
+         */
+        if (b.hasPiece(newRow, newCol)) {
+            if (b.getPiece(newRow, newCol).getColor().equals(getColor())) {
+                return false;
+            }
+        }
+
+        if (this.getColor().equals(Piece.WHITE)) {
+            if (oldRow == 6) { //Cho phép tốt di chuyển 2 ô khi quân tốt đang ở vị trí ban đầu
+                if (Math.abs(newCol - oldCol) == 1 && newRow == oldRow - 1 && b.hasPiece(newRow, newCol)) {
+                    return true;
+                } else if (newCol == oldCol && newRow == oldRow - 1 && !b.hasPiece(newRow, newCol)) {
+                    return true;
+                } else if (newCol == oldCol && newRow == oldRow - 2 && !b.hasPiece(newRow, newCol) && !b.hasPiece(newRow + 1, newCol)) {
+                    return true;
+                }
+                return false;
+            } else { //Khi quân tốt không còn ở vị trí ban đầu
+                if (Math.abs(newCol - oldCol) == 1 && newRow == oldRow - 1 && b.hasPiece(newRow, newCol)) {
+                    return true;
+                } else if (newCol == oldCol && newRow == oldRow - 1 && !b.hasPiece(newRow, newCol)) {
+                    return true;
+                }
+                return false;
+            }
+        } else {
+            if (oldRow == 1) { //Cho phép tốt di chuyển 2 ô khi quân tốt đang ở vị trí ban đầu
+                if (Math.abs(newCol - oldCol) == 1 && newRow == oldRow + 1 && b.hasPiece(newRow, newCol)) {
+                    return true;
+                } else if (newCol == oldCol && newRow == oldRow + 1 && !b.hasPiece(newRow, newCol)) {
+                    return true;
+                } else if (newCol == oldCol && newRow == oldRow + 2 && !b.hasPiece(newRow, newCol) && !b.hasPiece(newRow - 1, newCol)) {
+                    return true;
+                }
+                return false;
+            } else { //Khi quân tốt không còn ở vị trí ban đầu
+                if (Math.abs(newCol - oldCol) == 1 && newRow == oldRow + 1 && b.hasPiece(newRow, newCol)) {
+                    return true;
+                } else if (newCol == oldCol && newRow == oldRow + 1 && !b.hasPiece(newRow, newCol)) {
+                    return true;
+                }
+                return false;
+            }
+        }
     }
 }
