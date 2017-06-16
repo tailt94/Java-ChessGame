c58f3d705472227af8fd5e526cc19c30bf90cbf0
Lê Tuấn Tài <tuantai0625@gmail.com>
2017-06-16 17:29:34 +0700

    Update Knight legal move check

--- src/com/tuantai0625/chessgame/model/Knight.java
+++ src/com/tuantai0625/chessgame/model/Knight.java
@@ -15,6 +15,19 @@ public class Knight extends Piece {
 
     @Override
     public boolean isLegalMove(ChessBoard b, int newRow, int newCol) {
-        return true;
+        int oldRow = this.tileOn.getRow();
+        int oldCol = this.tileOn.getCol();
+
+        /*
+         * Kiểm tra ô được di chuyển tới có tồn tại quân cờ cùng phe hay không
+         */
+        if (b.hasPiece(newRow, newCol)) {
+            if (b.getPiece(newRow, newCol).getColor().equals(getColor())) {
+                return false;
+            }
+        }
+
+        return ((Math.abs(newRow - oldRow) == 2 && Math.abs(newCol - oldCol) == 1)
+                || (Math.abs(newRow - oldRow) == 1 && Math.abs(newCol - oldCol) == 2));
     }
 }
