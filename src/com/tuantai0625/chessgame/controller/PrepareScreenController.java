a581a1a2370517d57a3894a8a8e16e4a42a64633
Lê Tuấn Tài <tuantai0625@gmail.com>
2017-06-17 20:58:17 +0700

    Handle screen changing

--- src/com/tuantai0625/chessgame/controller/PrepareScreenController.java
+++ src/com/tuantai0625/chessgame/controller/PrepareScreenController.java
@@ -1,13 +1,20 @@
 package com.tuantai0625.chessgame.controller;
 
+import com.tuantai0625.chessgame.Main;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -37,7 +44,17 @@ public class PrepareScreenController implements Initializable {
     }
 
     @FXML
-    void onButtonStartClick(ActionEvent event) {
-        
+    void onButtonStartClick(ActionEvent event) throws IOException {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Parent root = FXMLLoader.load(Main.class.getResource("view/main_screen.fxml"));
+        Scene scene = stage.getScene();
+        if (scene == null) {
+            scene = new Scene(root);
+            stage.setScene(scene);
+        } else {
+            stage.getScene().setRoot(root);
+        }
+        stage.show();
+        stage.sizeToScene();
     }
 }
